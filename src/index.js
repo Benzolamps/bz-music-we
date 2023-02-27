05ef908ad7cfb0bb298e5856770ba4a8c8e8e7ee
Benzolamps <gwy9826@163.com>
2023-02-28 00:43:26 +0800

    添加

--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
 import "ecma-proposal-math-extensions";
 import "./presetBase";
 import Visualizer from "./visualizer";
-import {convertPreset} from '../../milkdrop-preset-converter/dist/milkdrop-preset-converter.min'
 
 export default class Butterchurn {
   static createVisualizer(context, canvas, opts) {
     return new Visualizer(context, canvas, opts);
-  }
-  
-  static convertPreset(text) {
-    return convertPreset(text);
   }
 }
